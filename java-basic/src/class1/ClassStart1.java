e0e880d4fd751f463c8cccc6cd5b2f2884150d42
itonse <govl3dnjs@gmail.com>
2024-06-14 13:11:48 +0900

    feat: 클래스 도입 전 코드

---
+++ java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,22 @@
+package class1;
+
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String st1Name = "학생1";
+        int st1Age = 15;
+        int st1Grade = 90;
+
+        String st2Name = "학생2";
+        int st2Age = 16;
+        int st2Grade = 80;
+
+        String st3Name = "학생2";
+        int st3Age = 17;
+        int st3Grade = 80;
+
+        System.out.println("이름: " + st1Name + " 나이: " + st1Age + " 성적: " + st1Grade);
+        System.out.println("이름: " + st2Name + " 나이: " + st2Age + " 성적: " + st2Grade);
+        System.out.println("이름: " + st3Name + " 나이: " + st2Age + " 성적: " + st3Grade);
+    }
+}
